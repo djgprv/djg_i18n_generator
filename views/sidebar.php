632b709c654b0619d0b8b949d66c6f8c1c35675d
djgprv <djgprv@gmail.com>
2013-05-02 15:40:34 +0200

    sidebar uri

--- views/sidebar.php
+++ views/sidebar.php
@@ -18,11 +18,10 @@ if (!defined('IN_CMS')) { exit(); }
  * @license http://www.gnu.org/licenses/gpl.html GPLv3 license
  */
 ?>
-<p class="button"><a href="<?php echo get_url('plugin/djg_i18n_generator/en_language'); ?>"><img src="<?php echo URL_PUBLIC; ?>wolf/plugins/djg_i18n_generator/images/32_en.png" align="middle" alt="settings icon" /> <?php echo __('English language'); ?></a></p>
-<p class="button"><a href="<?php echo get_url('plugin/djg_i18n_generator/other_languages'); ?>"><img src="<?php echo URL_PUBLIC; ?>wolf/plugins/djg_i18n_generator/images/32_euro.png" align="middle" alt="settings icon" /> <?php echo __('Other languages'); ?></a></p>
-<p class="button"><a href="<?php echo get_url('plugin/djg_i18n_generator/settings'); ?>"><img src="<?php echo URL_PUBLIC; ?>wolf/plugins/djg_i18n_generator/images/32_settings.png" align="middle" alt="settings icon" /> <?php echo __('Settings'); ?></a></p>
-<p class="button"><a href="<?php echo get_url('plugin/djg_i18n_generator/documentation/'); ?>"><img src="<?php echo URL_PUBLIC; ?>wolf/plugins/djg_i18n_generator/images/32_documentation.png" align="middle" alt="documentation icon" /> <?php echo __('Documentation'); ?></a></p>
-<div class="box">
+<p class="button"><a href="<?php echo get_url('plugin/djg_i18n_generator/en_language'); ?>"><img src="<?php echo PLUGINS_URI; ?>djg_i18n_generator/images/32_en.png" align="middle" alt="settings icon" /> <?php echo __('English language'); ?></a></p>
+<p class="button"><a href="<?php echo get_url('plugin/djg_i18n_generator/other_languages'); ?>"><img src="<?php echo PLUGINS_URI; ?>djg_i18n_generator/images/32_euro.png" align="middle" alt="settings icon" /> <?php echo __('Other languages'); ?></a></p>
+<p class="button"><a href="<?php echo get_url('plugin/djg_i18n_generator/settings'); ?>"><img src="<?php echo PLUGINS_URI; ?>djg_i18n_generator/images/32_settings.png" align="middle" alt="settings icon" /> <?php echo __('Settings'); ?></a></p>
+<p class="button"><a href="<?php echo get_url('plugin/djg_i18n_generator/documentation'); ?>"><img src="<?php echo PLUGINS_URI; ?>djg_i18n_generator/images/32_documentation.png" align="middle" alt="documentation icon" /> <?php echo __('Documentation'); ?></a></p><div class="box">
     <h2><?php echo __('Dialog window'); ?></h2>
 	<p>_</p>
 </div>
